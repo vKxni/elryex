f0fc0e7b19aef95caba3e91ec53a679b6aa135d0
Konrad570 <Konradtoesch@icloud.com>
2021-10-09 17:30:47 +0200

    added list comprehensions!

---
+++ lib/Admin/looping.ex
@@ -0,0 +1,12 @@
+# to loop trough i, you can do/use list comprehensions!
+
+  # for i <- [1, 2, 3], do: i
+  # iex> [1, 2, 3]
+
+#  -----------------------------
+
+
+# for higher numbers, multiply it!
+
+# for i <- [1, 2, 3], do: i*i
+  # iex> [1, 4, 9]
