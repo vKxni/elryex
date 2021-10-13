4f820545a27e0f39c2b2543a929d036b441774e9
Konrad570 <Konradtoesch@icloud.com>
2021-10-13 20:27:31 +0200

    added multiply function

--- lib/Math/anonymous-func.ex
+++ lib/Math/anonymous-func.ex
@@ -1,6 +1,7 @@
 defmodule Anonymous do
   def multiply do
     _multiply = fn (a, b) -> (a * b) end
+    5 * 5
     # iex> multiply.(5, 5) || we need the dot (.) after multiply to be able to call anonymous functions. Thats how we use them
     # => 25
   end
