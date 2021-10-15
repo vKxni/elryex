eee10a393f265e911e719b26d1ca64efc36fb742
Konrad570 <Konradtoesch@icloud.com>
2021-10-15 21:52:42 +0200

    removed hard coded anonymous function

--- lib/Math/anonymous-func.ex
+++ lib/Math/anonymous-func.ex
@@ -1,8 +1,14 @@
 defmodule Anonymous do
-  def multiply do
+  def multiply(number1, number2) do
     multiply = fn (a, b) -> (a * b) end
-    multiply.(10, 50)
-    # iex> multiply.(5, 5) || we need the dot (.) after multiply to be able to call anonymous functions. Thats how we use them
-    # => 25
+    multiply.(number1, number2)
   end
 end
+
+
+
+# The idea behind is (basic):
+
+# iex> multiply = fn (a, b) -> (a * b) end
+# iex> multiply.(5, 5) || we need the dot (.) after multiply to be able to call anonymous functions. Thats how we use them
+# => 25
