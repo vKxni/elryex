eec1344c5b1db3f9511f03806d50ea149aa2b6c5
Konrad570 <Konradtoesch@icloud.com>
2021-12-11 15:58:43 +0100

    added second method

--- lib/Messages/vocals.ex
+++ lib/Messages/vocals.ex
@@ -1,11 +1,18 @@
 defmodule Vocal do
   def remove do
-    string = "Hallo, ich bin die Ulrike und auch dabei!"
-    string
+    sentence = "Hallo, ich bin die Ulrike und auch dabei!"
+    sentence
     |> String.graphemes()
     |> Enum.filter(fn char -> String.downcase(char) not in ["a", "e", "i", "o", "u"] end)
     |> Enum.join()
     |> IO.puts()
-    IO.puts(string)
+    IO.puts(sentence)
+  end
+
+  def remove2 do
+    sentence = "Hallo, ich bin die Ulrike und auch dabei!"
+    String.replace(sentence, ~r/[aeiou]/i, "")
+    |> IO.puts()
+    IO.puts(sentence)
   end
 end
