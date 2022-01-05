6fa66bc4d6519d427a4bd4d36ed08b9501a782aa
Konrad570 <Konradtoesch@icloud.com>
2022-01-05 18:15:30 +0100

    updated loops

--- lib/Admin/looping.ex
+++ lib/Admin/looping.ex
@@ -10,3 +10,15 @@
 
 # for i <- [1, 2, 3], do: i*i
   # iex> [1, 4, 9]
+
+defmodule Loop do
+  def loop do
+    for i <- [1, 2, 3], do: i # *i, whatever.
+  end
+end
+
+# spam loop:
+
+# fn is a anonymous function with the variable number, -> do something, in our case Print it.
+# Enum.each(0..100, fn number -> IO.puts(number) end)
+# (https://inquisitivedeveloper.com/lwm-elixir-73/)
