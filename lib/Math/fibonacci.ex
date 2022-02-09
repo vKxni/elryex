a44af1c18e2b9ca0152196a897833ee4f4826872
vKxni <Konradtoesch@icloud.com>
2022-02-09 18:24:34 +0100

    update

--- lib/Math/fibonacci.ex
+++ lib/Math/fibonacci.ex
@@ -1,10 +1,31 @@
 defmodule Fibo do
-  def fib(amount) do
-    list = for n <- 1..amount, into: [], do: fib_calc(n)
-    IO.inspect list
+  def fib(amount, results \\ [], current \\ 1)
+
+  def fib(amount, results, current) when current > amount, do: Enum.reverse(results)
+
+  def fib(amount, [prev, prev2 | rest], current) do
+    next = prev + prev2
+    fib(amount, [next, prev, prev2 | rest], current + 1)
   end
 
-  defp fib_calc(0), do: 0
-  defp fib_calc(1), do: 1
-  defp fib_calc(n), do: fib_calc(n-1) + fib_calc(n-2)
+  def fib(amount, [prev | rest], current) do
+    next = prev
+    fib(amount, [next, prev | rest], current + 1)
+  end
+
+  def fib(amount, [], current) do
+    fib(amount, [1], current + 1)
+  end
 end
+
+# old
+#defmodule Fibo do
+#  def fib(amount) do
+#    list = for n <- 1..amount, into: [], do: fib_calc(n)
+#    IO.inspect list
+#  end
+
+# defp fib_calc(0), do: 0
+#  defp fib_calc(1), do: 1
+#  defp fib_calc(n), do: fib_calc(n-1) + fib_calc(n-2)
+#end
