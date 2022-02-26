d3bcabec05e9ea32dcd5172963623f0385a52944
vKxni <Konradtoesch@icloud.com>
2022-02-26 14:59:21 +0100

    docs

--- lib/Messages/gtn.ex
+++ lib/Messages/gtn.ex
@@ -9,8 +9,25 @@ defmodule Gtn do
 
     case guess do
       :error -> IO.puts("This is not a number!")
-      {number, _} when number == random -> IO.puts("You guessed it correctly!")
+
+      # using guards
+      #  {number, _} when number == random -> IO.puts("You guessed it correctly!")
+
+      # could be written with the pin operator
+      {^random, _} -> IO.puts("You guessed it correctly!")
+
       _ -> IO.puts("Wrong number, it was #{random}")
     end
   end
 end
+
+# About the docs above:
+
+# elixir takes the value in case value do and tries to fit it into each pattern in each clause, then checks if any associated guards are also true
+# so, for {random, _} when random == guess, elixir first fits guess into that tuple; guess looks like:
+
+                                # {2, “\n”}
+# so the pattern matches and it binds random to 2 (shadowing the original value of random) and throws away the other part.
+# now it checks the guard: random is 2 in this scope, but guess is still {2, “\n”}, so 2 == {2, “\n”} is false, and it skips to the next clause ...
+# if you want to pattern match to a variable’s bound value instead of binding a new value to it, use the pin operator (^)
+# in front of the variable name, like {^random, _}; so you won’t need a guard in this case
